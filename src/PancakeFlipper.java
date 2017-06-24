87879cb048f7926972d41a1ed086c52eb2ed59fd
Young Park <young_park@brown.edu>
2017-06-24 12:25:57 +0900

    Fixed a problem with algorithm. Program works as intended.

--- src/PancakeFlipper.java
+++ src/PancakeFlipper.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,8 +17,11 @@ public class PancakeFlipper {
 
     private int howManyFlips() {
         checkBottom();
+        if (this.pancakeStatus.isEmpty()) {
+            return 0;
+        }
         boolean changeOrientation = this.pancakeStatus.get(0);
-        int counter = 0;
+        int counter = 1;
         for (int i = 0; i < this.pancakeStatus.size(); i++) {
             if (this.pancakeStatus.get(i) != changeOrientation) {
                 counter++;
@@ -37,29 +41,14 @@ public class PancakeFlipper {
     }
 
     public static void main(String[] args) {
-        String s = "++-++++---";
-        ArrayList<Boolean> pancakeStatus = new ArrayList<Boolean>();
-        for (int j = 0; j < s.length(); j++) {
-            String temp = s.substring(j, j+1);
-            System.out.println(temp);
-            if (temp.equals("+")) {
-                pancakeStatus.add(true);
-            } else if (temp.equals("-")) {
-                pancakeStatus.add(false);
-            }
-        }
-        PancakeFlipper l = new PancakeFlipper(pancakeStatus);
-        System.out.println(l.howManyFlips());
-    }
-
-    /*public static void main(String[] args) {
-        File input = new File("B-small-practice.in");
+        File input = new File("B-large-practice.in");
         try {
             Scanner sc = new Scanner(input);
+            PrintWriter pw = new PrintWriter("output-large.out");
             int testCases = sc.nextInt();
             for (int i = 0; i < testCases; i++) {
-                String s = sc.nextLine();
-                ArrayList<Boolean> pancakeStatus = new ArrayList<Boolean>();
+                ArrayList<Boolean> pancakeStatus = new ArrayList<>();
+                String s = sc.next();
                 for (int j = 0; j < s.length(); j++) {
                     String temp = s.substring(j, j+1);
                     if (temp.equals("+")) {
@@ -67,11 +56,13 @@ public class PancakeFlipper {
                     } else if (temp.equals("-")) {
                         pancakeStatus.add(false);
                     }
-                    PancakeFlipper l = new PancakeFlipper(pancakeStatus);
                 }
+                PancakeFlipper l = new PancakeFlipper(pancakeStatus);
+                pw.println("Case #" + (i+1) + ": " + l.howManyFlips());
             }
+            pw.close();
         } catch (FileNotFoundException a) {
             System.out.println("File not found.");
         }
-    }*/
+    }
 }
